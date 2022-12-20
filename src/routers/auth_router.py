deed45c3b6e71d482b372e8911df68a7ebc4b0fe
Philane Msibi <philane@epicdev.co.za>
2022-12-20 20:33:00 +0200

    Login handler WIP

--- src/routers/auth_router.py
+++ src/routers/auth_router.py
@@ -2,13 +2,31 @@ from fastapi import APIRouter, status, HTTPException
 
 from datetime import datetime
 
-from ..schemas.user_schema import UserRegisterSchema, UserRegisterResponseSchema
+from ..schemas.user_schema import UserRegisterSchema, UserRegisterResponseSchema, UserLoginSchema, UserLoginResponseSchema
 
 from ..models.user import userModel
 
 from ..utils.password_utils import passwordUtils
 
 auth_router = APIRouter()
+
+@auth_router.post("/login", summary="Logs In A User", status_code=status.HTTP_201_CREATED, response_model=UserLoginResponseSchema)
+async def log_user_in(user: UserLoginSchema):
+    
+    user_exists = await userModel.by_email(user.email)
+    
+    if not user_exists:
+        HTTPException(status_code=404, detail=f"User With {user.email} Not Found")
+        
+    password_match = await passwordUtils.verify_password(user.password, user_exists.password)
+    
+    if not password_match:
+        HTTPException(status_code=404, detail=f"User With {user.email} Not Found P")
+
+        
+    return UserLoginResponseSchema(message="User Found")
+    
+    
 
 @auth_router.post("/register", summary="Registers A New User", status_code=status.HTTP_201_CREATED, response_model=UserRegisterResponseSchema)
 async def register_user(user: UserRegisterSchema):
--- src/schemas/user_schema.py
+++ src/schemas/user_schema.py
@@ -55,3 +55,40 @@ class UserRegisterResponseSchema(BaseModel):
                 "message": "Message Added Succesfully"
             }
         }
+        
+        
+        
+class UserLoginSchema(BaseModel):
+    """
+    Defines the schema of the request body when the user is logging in
+
+    Args:
+        BaseModel
+    """
+    email: EmailStr = Field(...,description="The Email Address Of The User")
+    password: str = Field(..., description="The Password Of The User", min_length=8, max_length=20)
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "jane@doe.com",
+                "password": "J@n3D03!"
+            }
+        }
+        
+class UserLoginResponseSchema(BaseModel):
+    """
+    Schema that defines the schema of the response after the user has been logging In
+
+    Args:
+        BaseModel
+    """
+    message: str = Field(..., description="Message of outcome")
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "message": "Message Added Succesfully"
+            }
+        }
+        
